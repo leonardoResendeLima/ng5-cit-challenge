dcdaffd70ae983faabc1c8c835fea87a31d408bd
Leonardo Lima <leonardoresendelima@gmail.com>
2018-02-28 15:53:28 -0300

    Ajustando template de detalhes

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -1,8 +1,14 @@
+// Angular Dependencies
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
 import { HttpClient } from '@angular/common/http';
+
+// 3rd Party Libraries Dependencies
 import * as _ from "lodash"
+
+// Interfaces
+import { MarvelHeroes } from "../../assets/interfaces/marvelHeroes";
 
 @Component({
 	selector: 'app-details',
@@ -14,18 +20,23 @@ export class DetailsComponent implements OnInit {
 	characterId: number;
 	character;
 
-	constructor(private route: ActivatedRoute, private http : HttpClient) {
+	constructor(private route: ActivatedRoute, private http: HttpClient, private router: Router) {
+		// Retrieving id parameter
 		this.route.params.subscribe(res => {
-			console.log(res.id)
-			this.characterId = res.id
+			if (!res.id)
+				this.router.navigate(['']);
+			else
+				this.characterId = res.id;
 		})
 
-		this.http.get("http://gateway.marvel.com/v1/public/characters/" + this.characterId + "?ts=1000&apikey=8d4fb63f32f1e6c7e6ea1614c26c306d&hash=1c9c679cc29228a724e6be0fe57892e6").subscribe(x => {
-			this.character = _.first(x.data.results);
+		this.http.get<MarvelHeroes>("http://gateway.marvel.com/v1/public/characters/" + this.characterId + "?ts=1000&apikey=8d4fb63f32f1e6c7e6ea1614c26c306d&hash=1c9c679cc29228a724e6be0fe57892e6").subscribe(x => {
+			var a: MarvelHeroes = x;
+			this.character = a.data.results;
 		})
 	}
 
 	ngOnInit() {
+
 	}
 
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { md5 } from "md5";
+import { MarvelHeroes } from "../../assets/interfaces/marvelHeroes";
 
 @Component({
 	selector: 'app-home',
@@ -14,26 +15,20 @@ export class HomeComponent implements OnInit {
 	timestamp = new Date().getTime().toString();
 	// hash = md5(this.timestamp + this._privateKey + this._publicKey)
 
-	// posts: Post[];
-
 	characters;
+
 	constructor(private http: HttpClient) {
-		
+
 		// this.http.get<Post[]>("https://jsonplaceholder.typicode.com/posts").subscribe(x => {
 		// 	this.posts = x;
 		// 	console.log(this.posts.length);
 		// })
 	}
 	ngOnInit() {
-		this.http.get("http://gateway.marvel.com/v1/public/characters?ts=1000&apikey=8d4fb63f32f1e6c7e6ea1614c26c306d&hash=1c9c679cc29228a724e6be0fe57892e6").subscribe(x => {
-			this.characters = x.data.results;
+		this.http.get<MarvelHeroes>("http://gateway.marvel.com/v1/public/characters?ts=1000&apikey=8d4fb63f32f1e6c7e6ea1614c26c306d&hash=1c9c679cc29228a724e6be0fe57892e6").subscribe(x => {
+			var a : MarvelHeroes = x;
+			this.characters = a.data.results;
+
 		})
 	}
-}
-
-interface Post {
-	userId: number,
-	id: number,
-	title: string,
-	body: string
 }
