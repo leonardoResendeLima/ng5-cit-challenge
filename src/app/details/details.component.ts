a1048c0686826fd9b3bfaf9987d0037be0611ccc
Leonardo Lima <leonardoresendelima@gmail.com>
2018-02-28 16:22:16 -0300

    parametrizando parâmetros get

--- src/app/details/details.component.ts
+++ src/app/details/details.component.ts
@@ -2,7 +2,7 @@
 import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Router } from '@angular/router';
-import { HttpClient } from '@angular/common/http';
+import { HttpClient, HttpParams } from '@angular/common/http';
 
 // 3rd Party Libraries Dependencies
 import * as _ from "lodash"
@@ -28,13 +28,20 @@ export class DetailsComponent implements OnInit {
 			else
 				this.characterId = res.id;
 		})
+		const params = new HttpParams()
+			.set("ts", "1000")
+			.set("apikey", "8d4fb63f32f1e6c7e6ea1614c26c306d")
+			.set("hash", "1c9c679cc29228a724e6be0fe57892e6")
+
+		this.http.get<MarvelHeroes>("http://gateway.marvel.com/v1/public/characters/" + this.characterId, { params })
+			.subscribe(data => {
+				var a: MarvelHeroes = data;
+				this.character = a.data.results;
+			})
 	}
 
 	ngOnInit() {
-		this.http.get<MarvelHeroes>("http://gateway.marvel.com/v1/public/characters/" + this.characterId + "?ts=1000&apikey=8d4fb63f32f1e6c7e6ea1614c26c306d&hash=1c9c679cc29228a724e6be0fe57892e6").subscribe(x => {
-			var a: MarvelHeroes = x;
-			this.character = a.data.results;
-		})
+
 	}
 
 }
